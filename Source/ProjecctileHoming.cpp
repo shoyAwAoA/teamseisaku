33ec5eebb678c9ee0bcc9b9d38a7c94ae71c1017
shoyAwAoA <2230166@ecc.ac.jp>
2024-07-10 12:22:02 +0900

    popopo

--- Source/ProjecctileHoming.cpp
+++ Source/ProjecctileHoming.cpp
@@ -40,59 +40,61 @@ void ProjectileHoming::Update(float elapsedTime)
 
     //旋回
     {
-        float turnSpeed = this->turnSpeed * elapsedTime;
-        //ターゲットまでのベクトルを算出
-        DirectX::XMVECTOR Position = DirectX::XMLoadFloat3(&position);
-        DirectX::XMVECTOR Target = DirectX::XMLoadFloat3(&target);
-        DirectX::XMVECTOR Vec = DirectX::XMVectorSubtract(Target,Position);
-
-        //ゼロベクトルでないなら回転処理
-        DirectX::XMVECTOR LengthSq = DirectX::XMVector3LengthSq(Vec);
-        float lengthSq;
-        DirectX::XMStoreFloat(&lengthSq, LengthSq);
-        if (lengthSq > 0.00001f)
-        {
-            //ターゲットまでのベクトルを単位ベクトル化(正規化？）
-            Vec = DirectX::XMVector3Normalize(Vec);
-
-            //向いている方向ベクトルを算出
-            DirectX::XMVECTOR Direction = DirectX::XMLoadFloat3(&direction);
-
-           // 前方方向ベクトルとターゲットまでのベクトルの内積（角度）を算出
-            DirectX::XMVECTOR Dot = DirectX::XMVector3Dot(Direction, Vec);
-
-            float dot;
-            DirectX::XMStoreFloat(&dot, Dot);//VECTORからFLOAT3に変換
-
-            //２つの単位ベクトルの角度が小さいほど
-            //1.0に近づくという性質を利用して回転速度を調整する
-            float rot = 1.0 - dot;
-            if (rot > turnSpeed)//turnSpeed//回転速度？
-            {
-                rot = turnSpeed;
-            }
 
 
+        //float turnSpeed = this->turnSpeed * elapsedTime;
+        ////ターゲットまでのベクトルを算出
+        //DirectX::XMVECTOR Position = DirectX::XMLoadFloat3(&position);
+        //DirectX::XMVECTOR Target = DirectX::XMLoadFloat3(&target);
+        //DirectX::XMVECTOR Vec = DirectX::XMVectorSubtract(Target,Position);
 
-            //回転角度があるなら回転処理をする
-            //  //任意軸回転
-            if (fabsf(rot)>0.0001f)//fabsfはfloat型の絶対値を計算するための関数
-            {
-                //回転軸を算出
-                DirectX::XMVECTOR Axis = DirectX::XMVector3Cross(Direction,Vec);
+        ////ゼロベクトルでないなら回転処理
+        //DirectX::XMVECTOR LengthSq = DirectX::XMVector3LengthSq(Vec);
+        //float lengthSq;
+        //DirectX::XMStoreFloat(&lengthSq, LengthSq);
+        //if (lengthSq > 0.00001f)
+        //{
+        //    //ターゲットまでのベクトルを単位ベクトル化(正規化？）
+        //    Vec = DirectX::XMVector3Normalize(Vec);
 
-                //回転軸と回転量から回転行列を算出
-                DirectX::XMMATRIX Rotation = DirectX::XMMatrixRotationAxis(Axis, rot);
+        //    //向いている方向ベクトルを算出
+        //    DirectX::XMVECTOR Direction = DirectX::XMLoadFloat3(&direction);
 
-                //現在の行列を回転させる
-                DirectX::XMMATRIX Transform = DirectX::XMLoadFloat4x4(&transform);
-                Transform = DirectX::XMMatrixMultiply(Transform, Rotation);
+        //   // 前方方向ベクトルとターゲットまでのベクトルの内積（角度）を算出
+        //    DirectX::XMVECTOR Dot = DirectX::XMVector3Dot(Direction, Vec);
 
-                    //回転後の前方方向を取り出し、単位ベクトル化する
-                Direction = DirectX::XMVector3Normalize(Transform.r[2]);
-                    DirectX::XMStoreFloat3(&direction, Direction);
-            }
-        }
+        //    float dot;
+        //    DirectX::XMStoreFloat(&dot, Dot);//VECTORからFLOAT3に変換
+
+        //    //２つの単位ベクトルの角度が小さいほど
+        //    //1.0に近づくという性質を利用して回転速度を調整する
+        //    float rot = 1.0 - dot;
+        //    if (rot > turnSpeed)//turnSpeed//回転速度？
+        //    {
+        //        rot = turnSpeed;
+        //    }
+
+
+
+        //    //回転角度があるなら回転処理をする
+        //    //  //任意軸回転
+        //    if (fabsf(rot)>0.0001f)//fabsfはfloat型の絶対値を計算するための関数
+        //    {
+        //        //回転軸を算出
+        //        DirectX::XMVECTOR Axis = DirectX::XMVector3Cross(Direction,Vec);
+
+        //        //回転軸と回転量から回転行列を算出
+        //        DirectX::XMMATRIX Rotation = DirectX::XMMatrixRotationAxis(Axis, rot);
+
+        //        //現在の行列を回転させる
+        //        DirectX::XMMATRIX Transform = DirectX::XMLoadFloat4x4(&transform);
+        //        Transform = DirectX::XMMatrixMultiply(Transform, Rotation);
+
+        //            //回転後の前方方向を取り出し、単位ベクトル化する
+        //        Direction = DirectX::XMVector3Normalize(Transform.r[2]);
+        //            DirectX::XMStoreFloat3(&direction, Direction);
+        //    }
+        //}
     }
 
     //オブジェクト行列の更新
