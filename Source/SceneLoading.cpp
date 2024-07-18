ec97fea22d071a2a30489c3186561480b44ab7e7
shoyAwAoA <2230166@ecc.ac.jp>
2024-07-18 10:28:28 +0900

    a

--- Source/SceneLoading.cpp
+++ Source/SceneLoading.cpp
@@ -8,7 +8,7 @@
 void SceneLoading::Initialize()
 {
     //スプライト初期化
-    sprite = new Sprite("Data/Sprite/setumee.png");
+    sprite = new Sprite("Data/Sprite/setumeee.png");
 
     //スレッド開始
     thread = new std::thread(LoadingThread, this);
